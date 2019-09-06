8a71589837911dd9d63ece72310703b403cdb3e3
pm-sharma <pmsharma1973990@gmail.com>
2019-09-07 01:09:53 +0530

    started with basics

---
+++ app.js
@@ -0,0 +1,61 @@
+
+// Simple time interval programme
+
+// var time =0;
+// var timer = setInterval(function(){
+//     time +=2;
+//     console.log(__dirname);
+//     console.log(__filename);
+//     console.log(time + " Hey How are you time");
+//     if(time>5){
+//         clearInterval(timer);
+//     }
+// },2000);
+
+
+
+
+
+// Setup http server
+
+// var http = require('http');
+// http.createServer(function (req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   res.end('Hey World How are you !');
+//   console.log("Hello");
+// }).listen(8080);
+
+
+
+
+
+//Normal Function Statement
+
+// function sayHi(){
+//     console.log("Hi");
+// }
+// sayHi();
+
+//function expression 
+// var sayBye = function(){
+//     console.log("Bye");
+// }
+// sayBye();
+
+// function callFunction(fun){
+//     fun();
+// }
+// callFunction(sayBye);
+
+
+
+
+
+// MODULE EXPORTS
+// var counter =  require('./count');
+
+// console.log(counter(["aea","12132","31123","232"]));
+
+
+
+
---
+++ count.js
@@ -0,0 +1,10 @@
+var counter = function(arr){
+    return "There are "+arr.length+"hey";
+
+};
+
+
+// console.log(counter(["da","pa","adad"]));
+
+
+module.exports = counter;
