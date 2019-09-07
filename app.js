5783b93a7559c328581ff9cb32cc5d260d356ee2
pm-sharma <pmsharma1973990@gmail.com>
2019-09-07 23:41:52 +0530

    routing programme added

--- app.js
+++ app.js
@@ -193,7 +193,6 @@
 
 // Serving JSON Data
 
-
 // var fs = require('fs');
 // var http = require('http');
 
@@ -210,6 +209,35 @@
 
 // server.listen(8000, '127.0.0.1');
 // console.log("Going to complete");
+
+
+
+
+
+
+// Basics Routing
+
+// var fs = require('fs');
+// var http = require('http');
+
+// var server = http.createServer(function (req, res) {
+//   console.log('request was made'+req.url);
+
+//   if(req.url === '/home' || req.url === '/'){
+//     res.writeHead(200,{'Content-Type': 'test/html'});
+//     fs.createReadStream(__dirname+'/index.html','utf8').pipe(res);
+    
+//   } else if(req.url === '/contact'){
+//     res.writeHead(200,{'Content-Type': 'test/html'});
+//     fs.createReadStream(__dirname+'/contact.html','utf8').pipe(res);
+//   }
+// });
+
+// server.listen(8000, '127.0.0.1');
+// console.log("Going to complete");
+
+
+
 
 
 
